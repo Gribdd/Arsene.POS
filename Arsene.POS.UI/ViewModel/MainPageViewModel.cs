d6b1b45eec5c5978557392cd96c42395a141ebe7
Allen <allenlozada24@gmail.com>
2025-03-26 14:11:06 +0800

    add: pdf generation

--- Arsene.POS.UI/ViewModel/MainPageViewModel.cs
+++ Arsene.POS.UI/ViewModel/MainPageViewModel.cs
@@ -5,6 +5,12 @@ using Arsene.POS.UI.Model;
 using Arsene.POS.UI.View;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
 
 namespace Arsene.POS.UI.ViewModel
 {
@@ -31,6 +37,39 @@ namespace Arsene.POS.UI.ViewModel
         async Task NavigateToAddTodoPage()
         {
             await Shell.Current.GoToAsync(nameof(AddTodoItem));
+        }
+
+        [RelayCommand]  
+        private void ManipulatePdf()
+        {
+
+            string dest = Path.Combine(FileSystem.Current.AppDataDirectory, "SalesInvoice.pdf");
+            using (PdfWriter writer = new PdfWriter(dest))
+            using (PdfDocument pdf = new PdfDocument(writer))
+            using (Document document = new Document(pdf))
+            {
+                List topLevel = new List();
+                ListItem topLevelItem = new ListItem();
+                topLevelItem.Add(new Paragraph().Add("Item 1"));
+                topLevel.Add(topLevelItem);
+
+                List secondLevel = new List();
+                secondLevel.Add("Sub Item 1");
+                ListItem secondLevelItem = new ListItem();
+                secondLevelItem.Add(new Paragraph("Sub Item 2"));
+                secondLevel.Add(secondLevelItem);
+                topLevelItem.Add(secondLevel);
+
+                List thirdLevel = new List();
+                thirdLevel.Add("Sub Sub Item 1");
+                thirdLevel.Add("Sub Sub Item 2");
+                secondLevelItem.Add(thirdLevel);
+
+                document.Add(topLevel);
+            }
+
+            // Show success message
+            Shell.Current.DisplayAlert("Success", $"PDF saved at {dest}", "OK");
         }
     }
 }
